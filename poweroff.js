c27d34a861f798d628a517e514ae703dc46687a6
chaitanya ivvala <chaitanyaivvala1996@gmail.com>
2018-05-04 21:11:45 +0200

    updated latest

---
+++ poweroff.js
@@ -0,0 +1,10 @@
+const Api = require('./api');
+
+if (!process.env.DEVICE_IP) {
+  console.log('Please set the IP address of your Sony audio device like this: DEVICE_IP=192.168.X.X node index.js');
+  process.exit();
+}
+
+const api = new Api(`http://${process.env.DEVICE_IP}:54480/sony`);
+
+api.powerOff();
